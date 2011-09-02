362670741f5ffaad92ecd1dc3a2b6fbf65ca4794
henryk-laptop <henryksarat@yahoo.com>
2011-09-02 17:55:48 -0500

    UOfMe: Deployed to EC2 and added automated email to entities.

---
+++ UniversityOfMe/Repositories/Email/EntityEmailRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using UniversityOfMe.Models;
+using System.Collections.Generic;
+using UniversityOfMe.Helpers;
+
+namespace UniversityOfMe.Repositories.Email {
+    public class EntityEmailRepository : IEmailRepository {
+        private AutomatedEmailEntities theEntities = new AutomatedEmailEntities();
+
+        public void AddEmailJob(string aFromEmail, string aToEmail, string aSubject, string aBody) {
+            EmailJob myEmailJob = EmailJob.CreateEmailJob(0, aFromEmail, aToEmail, aSubject, aBody, DateTime.UtcNow, false, false);
+            theEntities.AddToEmailJobs(myEmailJob);
+            theEntities.SaveChanges();
+        }
+
+        public IEnumerable<EmailJob> GetEmailJobsToBeSent() {
+            return (from e in theEntities.EmailJobs
+                    where e.PresentSent == false && e.PostsentSent == false
+                    select e);
+        }
+
+        public void MarkEmailPresentToTrue(int anId) {
+            EmailJob myEmailJob = GetEmailJob(anId);
+            myEmailJob.PresentSent = true;
+            theEntities.ApplyCurrentValues(myEmailJob.EntityKey.EntitySetName, myEmailJob);
+            theEntities.SaveChanges();
+        }
+
+        public void MarkEmailPostsentToTrue(int anId) {
+            EmailJob myEmailJob = GetEmailJob(anId);
+            myEmailJob.PostsentSent = true;
+            theEntities.ApplyCurrentValues(myEmailJob.EntityKey.EntitySetName, myEmailJob);
+            theEntities.SaveChanges();
+        }
+
+        private EmailJob GetEmailJob(int anId) {
+            return (from e in theEntities.EmailJobs
+                    where e.Id == anId
+                    select e).FirstOrDefault<EmailJob>();
+        }
+    }
+}
---
+++ UniversityOfMe/Repositories/Email/IEmailRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UniversityOfMe.Models;
+
+namespace UniversityOfMe.Repositories.Email {
+    public interface IEmailRepository {
+        void AddEmailJob(string aFromEmail, string aToEmail, string aSubject, string aBody);
+        IEnumerable<EmailJob> GetEmailJobsToBeSent();
+        void MarkEmailPresentToTrue(int anId);
+        void MarkEmailPostsentToTrue(int anId);
+    }
+}
